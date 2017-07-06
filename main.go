75f7597fd4f9b154178c15d44e557fb41a450d06
Rirush <endernyasha11@gmail.com>
2017-07-06 15:35:07 +0800

    Handing in separated func

--- main.go
+++ main.go
@@ -19,6 +19,19 @@ const (
 	envurl   = "URL"
 )
 
+var (
+	query = make(chan tgbotapi.MessageConfig)
+)
+
+func initquery(bot *tgbotapi.BotAPI) {
+	sender := time.NewTicker(time.Millisecond * 34)
+	go func() {
+		for range sender.C {
+			bot.Send(<-query)
+		}
+	}()
+}
+
 func help() string {
 	return "I can do this stuff:\n" +
 		"Show this help message with /help\n" +
@@ -26,20 +39,45 @@ func help() string {
 		"If you have any ideas, you can tell me about them by creating [issue](https://github.com/Rirush/RirushGoBot/issues) with 'Feature request' label"
 }
 
+func handle(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+	if update.Message == nil {
+		log.Printf("Update(%d) skipped because it isn't a message", update.UpdateID)
+	}
+
+	log.Printf("Message(%d) with text %q received from @%s:%d", update.UpdateID, update.Message.Text, update.Message.From.UserName, update.Message.From.ID)
+
+	if update.Message.IsCommand() {
+		log.Printf("Message(%d) is command", update.UpdateID)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+		msg.ParseMode = "markdown"
+		msg.DisableWebPagePreview = true
+
+		switch strings.ToLower(update.Message.Command()) {
+
+		case "start":
+			msg.Text = fmt.Sprintf("Hello, %s.\n"+
+				"I'm a bot built with [telegram-bot-api](https://github.com/go-telegram-bot-api/telegram-bot-api) and [Golang](https://golang.org).\n"+
+				"If you're interested in stuff that I can do - use /help.\n\n"+
+				"Also, I'm open-source, so you can find my source code [here](https://github.com/Rirush/RirushGoBot)", update.Message.From.FirstName)
+
+		case "help":
+			msg.Text = help()
+		}
+
+		query <- msg
+	}
+}
+
 // Running bot with webhook updates, status page, database and Keen.io analytics
 func production() {
 	bot, _ := tgbotapi.NewBotAPI(os.Getenv(envtoken))
 	bot.Debug = false
 
-	sender := time.NewTicker(time.Millisecond * 34)
-	query := make(chan tgbotapi.MessageConfig)
-	go func() {
-		for range sender.C {
-			bot.Send(<-query)
-		}
-	}()
+	initquery(bot)
 
 	bot.SetWebhook(tgbotapi.NewWebhook(os.Getenv(envurl) + os.Getenv(envtoken)))
+
+	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 
 	// Status "page"
@@ -66,34 +104,7 @@ func production() {
 	go router.Run("0.0.0.0:" + os.Getenv("PORT"))
 
 	for update := range updates {
-		go func() {
-			if update.Message == nil {
-				log.Printf("Update(%d) skipped because it isn't a message", update.UpdateID)
-			}
-
-			log.Printf("Message(%d) with text %q received from @%s:%d", update.UpdateID, update.Message.Text, update.Message.From.UserName, update.Message.From.ID)
-
-			if update.Message.IsCommand() {
-				log.Printf("Message(%d) is command", update.UpdateID)
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-				msg.ParseMode = "markdown"
-				msg.DisableWebPagePreview = true
-
-				switch strings.ToLower(update.Message.Command()) {
-
-				case "start":
-					msg.Text = fmt.Sprintf("Hello, %s.\n"+
-						"I'm a bot built with [telegram-bot-api](https://github.com/go-telegram-bot-api/telegram-bot-api) and [Golang](https://golang.org).\n"+
-						"If you're interested in stuff that I can do - use /help.\n\n"+
-						"Also, I'm open-source, so you can find my source code [here](https://github.com/Rirush/RirushGoBot)", update.Message.From.FirstName)
-
-				case "help":
-					msg.Text = help()
-				}
-
-				query <- msg
-			}
-		}()
+		go handle(bot, update)
 	}
 }
 
@@ -110,14 +121,7 @@ func development() {
 
 	log.Printf("Using account @%s", bot.Self.UserName)
 
-	// Message sender
-	sender := time.NewTicker(time.Millisecond * 34)
-	query := make(chan tgbotapi.MessageConfig)
-	go func() {
-		for range sender.C {
-			bot.Send(<-query)
-		}
-	}()
+	initquery(bot)
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 100
@@ -133,34 +137,7 @@ func development() {
 
 	// Messages polling and handling
 	for update := range updates {
-		go func() {
-			if update.Message == nil {
-				log.Printf("Update(%d) skipped because it isn't a message", update.UpdateID)
-			}
-
-			log.Printf("Message(%d) with text %q received from @%s:%d", update.UpdateID, update.Message.Text, update.Message.From.UserName, update.Message.From.ID)
-
-			if update.Message.IsCommand() {
-				log.Printf("Message(%d) is command", update.UpdateID)
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-				msg.ParseMode = "markdown"
-				msg.DisableWebPagePreview = true
-
-				switch strings.ToLower(update.Message.Command()) {
-
-				case "start":
-					msg.Text = fmt.Sprintf("Hello, %s.\n"+
-						"I'm a bot built with [telegram-bot-api](https://github.com/go-telegram-bot-api/telegram-bot-api) and [Golang](https://golang.org).\n"+
-						"If you're interested in stuff that I can do - use /help.\n\n"+
-						"Also, I'm open-source, so you can find my source code [here](https://github.com/Rirush/RirushGoBot)", update.Message.From.FirstName)
-
-				case "help":
-					msg.Text = help()
-				}
-
-				query <- msg
-			}
-		}()
+		go handle(bot, update)
 	}
 }
 
@@ -168,7 +145,6 @@ func main() {
 	if os.Getenv(envtoken) == "" {
 		log.Panic("TG_TOKEN isn't set.")
 	}
-
 	if os.Getenv("PRODUCTION") == "1" {
 		production()
 	} else {
